dc119bba7c3276d3f14301013389bbf0c8ca03fb
David Luz <davidmelo8@hotmail.com>
2015-03-13 16:50:30 -0300

    V.2.1

--- js/Game.js
+++ js/Game.js
@@ -42,44 +42,57 @@ game.load.image('erro','imgs/erro.png');
 
 function create(){
 
+// Gabarito
+
+
 //Definição das posições no grid
 posicao1 = new Object();
+posicao1.status = 0;
 posicao1.x = 25;
 posicao1.y = 50;
 
 posicao2 = new Object();
+posicao2.status = 0;
 posicao2.x = 150;
 posicao2.y = 50;
 
 posicao3 = new Object();
+posicao3.status = 0;
 posicao3.x = 275;
 posicao3.y = 50;
 
 posicao4 = new Object();
+posicao4.status = 0;
 posicao4.x = 400;
 posicao4.y = 50;
 
 posicao5 = new Object();
+posicao5.status = 0;
 posicao5.x = 525;
 posicao5.y = 50;
 
 posicao6 = new Object();
+posicao6.status = 0;
 posicao6.x = 25;
 posicao6.y = 100;
 
 posicao7 = new Object();
+posicao7.status = 0;
 posicao7.x = 150;
 posicao7.y = 100;
 
 posicao8 = new Object();
+posicao8.status = 0;
 posicao8.x = 275;
 posicao8.y = 100;
 
 posicao9 = new Object();
+posicao9.status = 0;
 posicao9.x = 400;
 posicao9.y = 100;
 
 posicao10 = new Object();
+posicao10.status = 0;
 posicao10.x = 525;
 posicao10.y = 100;
 
@@ -93,6 +106,7 @@ for(var i=1; i<11;i++) {
 var xPosition = eval('posicao'+i+'.x');
 var yPosition = eval('posicao'+i+'.y');
 drop[i] = game.add.sprite(xPosition, yPosition,'drop');
+drop[i].name = i;
 }
 
 //Gera os drags dinamicamente
@@ -103,25 +117,47 @@ var xPosition = eval('posicao'+i+'.x');
 var yPosition = eval('posicao'+i+'.y');
 var currentDrag = 'drag'+i;
 var currentDropHandler = 'dropHandler['+i+']';
-console.log(currentDropHandler);
 drag[i] = game.add.sprite(xPosition, yPosition+100,currentDrag);
 drag[i].inputEnabled = true;
 drag[i].input.enableDrag(true);
 drag[i].events.onDragStop.add(largar, this); // o this já indica que a função será aplicada neste objeto
-	}
+drag[i].name = i;
+drag[i].resposta = 0;
+
+}
 
 function largar(dropedItem){
+
+              
 				for (var i=1; i<11; i++){
-                						
+                	
+                	//Executa se ocorrer a intesecção e se status da posição está como 0
+        				
 				    var boundsA = drop[i].getBounds();
 					var boundsB = dropedItem.getBounds();
-
 					if(Phaser.Rectangle.intersects(boundsA, boundsB)){
-    				dropedItem.x = drop[i].x;
+					dropedItem.x = drop[i].x;
     				dropedItem.y = drop[i].y;
-    				}
+    				
+    		
+    				}    
+    				
+				
+				}
+
+				//Testa se largou sobre outro drag
+
+				for(var i=1; i<11; i++){
+					var boundsA = dropedItem.getBounds();
+					var boundsB = drag[i].getBounds();
+					if(Phaser.Rectangle.intersects(boundsA, boundsB)&& boundsA!=boundsB){
+						dropedItem.y = dropedItem.y+20;
+					}
 
 				}
+
+		
+			
 
 			}
 
@@ -143,13 +179,41 @@ erro[i].visible = false;
 
 responder = game.add.sprite(posicao10.x,300, 'responder');
 responder.inputEnabled = true;
+responder.events.onInputDown.add(enviarResposta, this);
+
+
+function enviarResposta(){
+
+// Alterar valores aqui para mudar gabarito
+
+// Resposta 1***************
+
+for(var i=1; i<=10; i++){
+if(drag[i].y == 50 || drag[1].y == 100) 
+
+drag[i].resposta = 1;
+  
+else
+drag[i].resposta = 0;  
+}
+
+
+
+}
+//**************************
+
+
+
 
 } //<--Create acaba aqui	
 
 
 function update(){
 
-	
+
+
+
+
 }
 
 function goToPlay(){
